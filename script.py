fc311931f3d61d6ff33f1b0fc26710135e9d167f
CASPER_LEB <77207604+casperLEB@users.noreply.github.com>
2021-01-10 01:39:47 +0200

    Create script.py

---
+++ script.py
@@ -0,0 +1,45 @@
+import os
+os.system("clear")
+
+from termcolor import *
+color=colored("_____ _____ _____ _____ _____ _____ _____ _____ _____ _____ _____ _____ _____
+|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|","purple")
+
+from termcolor import *
+color=colored(" ___ __ _ ___ _ __   ___ _ __
+ / __/ _` / __| '_ \ / _ \ '__|
+| (_| (_| \__ \ |_) |  __/ |
+ \___\__,_|___/ .__/ \___|_|
+              |_| ","red")
+
+from termcolor import *
+color=colored("_____ _____ _____ _____ _____ _____ _____ _____ _____ _____ _____ _____ _____
+|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|","purple")
+
+import os
+os.system("pkg install update -y")
+
+import os
+os.system("pkg install upgrade -y")
+
+from termcolor import *
+color=colored("_   _
+ _ __  _   _| |_| |__   ___  _ __
+| '_ \| | | | __| '_ \ / _ \| '_ \
+| |_) | |_| | |_| | | | (_) | | | |
+| .__/ \__, |\__|_| |_|\___/|_| |_|
+|_|    |___/",green")
+
+
+import os
+os.system("pkg install python -y")
+
+import os
+os.system("pkg install git -y")
+
+import os
+os.system("pkg install openssh -y")
+
+from termcolor import *
+
+color=colored("bye","green")
