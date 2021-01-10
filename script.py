ceea2985c7654a74a87d617fb772d5b550c7da11
CASPER_LEB <77207604+casperLEB@users.noreply.github.com>
2021-01-10 02:02:10 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,6 @@
 import os
 os.system("clear")
-print("_____ _____ _____ _____ _____ _____ _____ _____ _____ _____ _____ _____ _____
-|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|")
+print("♪♥♪♥♪♥♪♥♪♥")
 print(" __________________
  / __/ _` / __| '_ \ / _ \ '__|
 | (_| (_| \__ \ |_) |  __/ |
@@ -16,13 +15,12 @@ os.system("pkg install update -y")
 import os
 os.system("pkg install upgrade -y")
 
-from termcolor import *
-color=colored("_   _
+print ("_   _
  _ __  _   _| |_| |__   ___  _ __
 | '_ \| | | | __| '_ \ / _ \| '_ \
 | |_) | |_| | |_| | | | (_) | | | |
 | .__/ \__, |\__|_| |_|\___/|_| |_|
-|_|    |___/",green")
+|_|    |___/")
 
 
 import os
@@ -35,5 +33,4 @@ import os
 os.system("pkg install openssh -y")
 
 from termcolor import *
-
 color=colored("bye","green")
