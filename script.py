864aa58810c3a50e6a47afec23648d57ac394b23
CASPER_LEB <77207604+casperLEB@users.noreply.github.com>
2021-01-10 02:11:18 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,11 +1,10 @@
 import os
 os.system("clear")
 print("♪♥♪♥♪♥♪♥♪♥")
-print(" 
- / __/ _` / __| '_ \ / _ \ '__|
-| (_| (_| \__ \ |_) |  __/ |
- \___\__,_|___/ .__/ \___|_|
-                |_| ")
+print("/ __/ _` / __| '_ \ / _ \ '__|")
+print("| (_| (_| \__ \ |_) |  __/ |")
+print("\___\__,_|___/ .__/ \___|_|")
+print("               |_| ")
 
 print("♥♪♥♪♥♪♥♪♥♪♥")
 
@@ -13,14 +12,7 @@ import os
 os.system("pkg install update -y")
 
 import os
-os.system("pkg install upgrade -y")
-
-print ("_   _
- _ __  _   _| |_| |__   ___  _ __
-| '_ \| | | | __| '_ \ / _ \| '_ \
-| |_) | |_| | |_| | | | (_) | | | |
-| .__/ \__, |\__|_| |_|\___/|_| |_|
-|_|    |___/")
+os.system("pkg install update -y")
 
 
 import os
