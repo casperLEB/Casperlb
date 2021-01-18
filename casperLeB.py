4b478dfabdbba0517eafbbb9cf0d5e333baedd8e
CASPER_LEB <77207604+casperLEB@users.noreply.github.com>
2021-01-19 01:11:19 +0200

    Update and rename casperLeb.py to casperLeB.py

---
+++ casperLeB.py
@@ -0,0 +1,27 @@
+
+print("👻💀👻💀👻💀👻💀👻💀👻💀👻💀👻💀👻💀👻💀👻💀👻💀👻💀👻")
+print(" / __/ _` / __| '_ \ / _ \ '__|")
+print("| (_| (_| \__ \|_) |  __/ |")
+print(" \___\__,_|___/ .__/ \___|_|")
+print("               |_| ")
+print("👻💀👻💀👻💀👻💀👻💀👻💀👻💀👻💀👻💀👻💀👻💀👻💀👻💀👻")
+
+import os
+os.system("pkg install fish")
+import os
+os.system("pkg update -y")
+import os
+os.system("pkg upgrade")
+
+import os
+os.system("pkg install python -y")
+
+import os
+os.system("pkg install git -y")
+
+import os
+os.system("pkg install openssh -y")
+
+import os
+os.system("pkg install ruby")
+
